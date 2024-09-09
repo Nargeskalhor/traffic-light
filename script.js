339a73e59a94efc42b97e27df99dfcfee9cb92cf
narges <13.narges.kalhor.82@gmail.com>
2024-09-10 01:24:22 +0330

    let trafficLightColor

---
+++ script.js
@@ -0,0 +1,10 @@
+let trafficLightColor = prompt("plase enter the desired color:");
+if (trafficLightColor === "red") {
+  console.log("stop");
+} else if (trafficLightColor === "yellow") {
+  console.log("slow down");
+} else if (trafficLightColor === "green") {
+  console.log("go");
+} else {
+  console.log("invalid color");
+}
